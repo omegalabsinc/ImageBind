140bbb3dcc2f89c9a0af00859e79b69ec9a8a9ad
Salman Shahid <salmanshahidzia@gmail.com>
2024-12-18 21:07:07 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='imagebind',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     description='A brief description of the package',
     long_description=open('README.md').read(),
